c326a36b69a0f6d3d8d85e0ca56a35bfca43a76e
Bophelo <mpilo@vivaldi.net>
2018-01-20 00:36:40 +0200

    Modal Component

---
+++ modal-component.js
@@ -0,0 +1,37 @@
+Vue.component('modal', {
+    props: ['title', 'body'],
+    data(){
+        return {
+            isVisible: true
+        };
+    },
+    template : `
+        <div class="modal is-active">
+          <div class="modal-background"></div>
+          <div class="modal-content">
+            <div class="box">
+                <p>
+                    <slot></slot>
+                </p>
+            </div>
+          </div>
+          <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+        </div>
+    `,
+
+    methods : {
+        hideModal(){
+            this.isVisible = false;
+        }
+    }
+});
+
+var vm = new Vue({
+
+    el: "#app",
+
+    data : {
+        showModal : false
+    }
+
+})
