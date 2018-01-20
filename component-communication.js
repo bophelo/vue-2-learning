fed32ce7e94310931e9e0950b07731878cd56c91
Bophelo <mpilo@vivaldi.net>
2018-01-20 02:33:02 +0200

    Event Dispatcher

--- component-communication.js
+++ component-communication.js
@@ -1,10 +1,11 @@
+window.Event = new Vue();
 Vue.component('coupon', {
     template : `
         <input placeholder="Enter your coupon code" @blur="onCouponApplied">
     `,
     methods : {
         onCouponApplied() {
-            this.$emit('applied');// can also pass in any data associated with it
+            Event.$emit('applied');// can also pass in any data associated with it
         }
     }
 });
@@ -14,6 +15,9 @@ var vm = new Vue({
     el: "#app",
     data : {
         couponApplied : false
+    },
+    created() {
+        Event.$on('applied', () => alert('Applying'));//any component can both fire and listen for other events
     },
     methods : {
         onCouponApplied() {
